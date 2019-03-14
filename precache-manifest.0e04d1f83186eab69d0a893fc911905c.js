150836437d9cb49437fb185f8a2a10e7eda7158d
PaphobGun <paphob.a@gmail.com>
2019-03-14 19:23:58 +0700

    Updates

---
+++ precache-manifest.0e04d1f83186eab69d0a893fc911905c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "343269eeb7f6cf125ce4",
+    "url": "/clientmanager/static/js/runtime~main.16ac5301.js"
+  },
+  {
+    "revision": "9c8cc2a87a3f47391495",
+    "url": "/clientmanager/static/js/main.e5e5d740.chunk.js"
+  },
+  {
+    "revision": "adbca101acd2ffb4306d",
+    "url": "/clientmanager/static/js/2.c2de7b53.chunk.js"
+  },
+  {
+    "revision": "9c8cc2a87a3f47391495",
+    "url": "/clientmanager/static/css/main.72d82db4.chunk.css"
+  },
+  {
+    "revision": "adbca101acd2ffb4306d",
+    "url": "/clientmanager/static/css/2.a1983ead.chunk.css"
+  },
+  {
+    "revision": "4081b55efa0bbc9bf2856dc01583e4e1",
+    "url": "/clientmanager/index.html"
+  }
+];
