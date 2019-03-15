fedba82d38fa3919884fe4071979cd17b1ec3cae
PaphobGun <paphob.a@gmail.com>
2019-03-15 23:34:17 +0700

    fixed

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <div className="main-body">
           <Navbar />
           <div className="container">
